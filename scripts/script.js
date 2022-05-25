294711d882b581d0bc33dc6ba002d7b281e1c07d
Dinesh Kumar <dkumar@epostglobalshipping.com>
2022-05-25 17:16:41 -0400

    Commited

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
 console.log('From Scripts: ', document);
 if ('serviceWorker' in navigator) 
 {
-     navigator.serviceWorker.register('/service-worker.js',{
-          scope:'/'
+     navigator.serviceWorker.register('/Lab1//service-worker.js',{
+          scope:'/Lab1/'
      }) .then((registration) => 
-     { console.log('Success. Scope:', registration); }) .
-=======
-if ('serviceWorker' in navigator) 
-{
-     navigator.serviceWorker.register('service-worker.js') .then((registration) => 
      { console.log('Success. Scope:', registration.scope); }) .
->>>>>>> 7fae0f4dc4a117151c1c65f14881d3f026866bdc
      catch((error) => { console.log('Failed. Error:', error); }); 
     }
+
